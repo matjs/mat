0f32186fff3f1ba191f9e6935ff776106ff32d26
naij <naijser@gmail.com>
2015-05-10 19:24:03 +0800

    增加url白名单控制

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ var error        = require('./lib/middleware/error')
 var forward      = require('./lib/middleware/forward')
 var serve        = require('./lib/middleware/static')
 var proxy        = require('./lib/middleware/proxy')
+var Url          = require('./lib/url')
 var mutil        = require('./util/mutil')
 var app          = koa()
 
 function Mat() {
   Orchestrator.call(this)
-  this.middleware = []
+  this.urls = []
 }
 
 util.inherits(Mat, Orchestrator)
 
 /**
- * Set up project env.
+ * 设置运行mat服务时需要的参数
  */
 Mat.prototype.env = function (env) {
   if (env.port) {
@@ -30,38 +31,21 @@ Mat.prototype.env = function (env) {
 }
 
 /**
- * Rename add to task.
+ * 将task设置为add方法的别名
  */
 Mat.prototype.task = Mat.prototype.add
 
 /**
- * Load middleware.
+ * url过滤器
  */
-Mat.prototype._middleware = function() {
-  app.use(error)
-
-  app.use(forward())
-
-  this.middleware.forEach(function (fn) {
-    if (mutil.isGeneratorFunction(fn)) {
-      app.use(fn)
-    }
-  })
-
-  app.use(serve(app.root || './'))
-
-  app.use(proxy())
+Mat.prototype.url = function (pattern) {
+  var url = new Url(pattern)
+  this.urls.push(url)
+  return url
 }
 
 /**
- * Store the given middleware `fn`.
- */
-Mat.prototype.use = function (fn) {
-  this.middleware.push(fn)
-}
-
-/**
- * Launch server.
+ * 启动mat服务
  */
 Mat.prototype.launch = function () {
   if (!this._start) {
@@ -72,6 +56,23 @@ Mat.prototype.launch = function () {
       console.log(chalk.green('mat is running'))
     })
   }
+}
+
+/**
+ * 加载中间件
+ */
+Mat.prototype._middleware = function() {
+  app.use(error)
+
+  app.use(forward())
+
+  this.urls.forEach(function (url) {
+    app.use(url.compose())
+  })
+
+  app.use(serve(app.root || './'))
+
+  app.use(proxy())
 }
 
 module.exports = new Mat()
